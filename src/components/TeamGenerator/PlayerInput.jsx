3767f750dbf4fb970700a1dafb4463b62fbe4fbd
Karol <kpulawski13@gmail.com>
2024-12-14 11:14:45 +0000

    implement playerInput component

--- src/components/TeamGenerator/PlayerInput.jsx
+++ src/components/TeamGenerator/PlayerInput.jsx
@@ -1,17 +1,31 @@
 import PropTypes from 'prop-types';
 
-const PlayerInput = ({ playerCount }) => (
-  <div className="space-y-2">
-    {Array.from({ length: playerCount }).map((_, index) => (
-      <input
-        key={index}
-        type="text"
-        placeholder={`Player ${index + 1}`}
-        className="w-full px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
-    ))}
-  </div>
-);
+const PlayerInput = ({
+  playerUsernames,
+  playerCount,
+  handlePlayerUsernameChange,
+}) => {
+  return (
+    <div className="space-y-2">
+      {Array.from({ length: playerCount }).map((_, index) => (
+        <input
+          key={index}
+          type="text"
+          value={playerUsernames[index] || ''}
+          onChange={(e) => {
+            handlePlayerUsernameChange(index, e.target.value);
+          }}
+          placeholder={`Player ${index + 1}`}
+          className="w-full px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+      ))}
+    </div>
+  );
+};
 
-PlayerInput.propTypes = { playerCount: PropTypes.number };
+PlayerInput.propTypes = {
+  playerUsernames: PropTypes.array,
+  playerCount: PropTypes.number,
+  handlePlayerUsernameChange: PropTypes.func.isRequired,
+};
 export default PlayerInput;
